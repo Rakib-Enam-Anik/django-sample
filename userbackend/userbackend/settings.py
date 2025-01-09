fd77f25bea12040c0504bd50adb6dd281d2382e3
Nazmul19285 <nazmulhossain19285@gmail.com>
2025-01-09 14:57:02 +0600

    Updated the settings

--- userbackend/userbackend/settings.py
+++ userbackend/userbackend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,11 +75,11 @@ TEMPLATES = [
     },
 ]
 
-ORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
-                        "http://127.0.0.1:3000",
-                        ]
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
+#                         "http://127.0.0.1:3000",
+#                         ]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
